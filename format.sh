c713792998a3d30a3f560c185aa44641e95c7fdf
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-09-20 18:56:08 -0700

    add tests dir to format.sh

--- format.sh
+++ format.sh
@@ -1,6 +1,7 @@
 clang-format -i include/*.h*
 clang-format -i include/*/*.h*
 clang-format -i src/*.c*
+clang-format -i tests/*.c*
 clang-format -i tools/*.c*
 clang-format -i tools-dev/*.c*
 clang-format -i examples/*.c*
